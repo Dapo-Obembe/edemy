50f03d3eabd8a0ee3d07a26fe031ae871034589d
dapo-obembe <obembedapo@gmail.com>
2022-07-25 16:00:33 +0100

    Added and styled the header component

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,35 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import "./Css/header.css";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <>
+      <header className="header">
+        <Navbar expand="lg">
+          <Container className="header-container">
+            <Navbar.Brand href="#home" id="logo">
+              e-Demy
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse
+              id="basic-navbar-nav"
+              className="justify-content-end"
+            >
+              <Nav className="me-auto nav-links">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">Teachers</Nav.Link>
+                <Nav.Link href="#link">Contact</Nav.Link>
+                <Nav.Link href="#link">Login</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </header>
+    </>
+  );
 }
 
 export default Header;
